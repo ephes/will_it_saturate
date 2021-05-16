3941bca442dddca8897cc8eb75ab3ca85b82df3d
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-05-16 14:34:28 +0200

    use standard logging to measure response times

--- will_it_saturate/django/settings.py
+++ will_it_saturate/django/settings.py
@@ -116,3 +116,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
