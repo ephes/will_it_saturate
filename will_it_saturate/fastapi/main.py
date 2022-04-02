abe0ab564877798b43565cb6b1a5009f0fc8fdb1
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-04-02 09:09:49 +0200

    dont print timings

--- will_it_saturate/fastapi/main.py
+++ will_it_saturate/fastapi/main.py
@@ -56,7 +56,7 @@ class MeasuringFileResponse(FileResponse):
                         }
                     )
                 elapsed = time.perf_counter() - start
-                print(f"{self.path},{elapsed}")
+                # print(f"{self.path},{elapsed}")
 
         if self.background is not None:
             await self.background()
